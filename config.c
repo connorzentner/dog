3c966eca6baa58140854d051e84a1cb28abfe1a6
connorzentner <c0nn0rz3ntn3r@gmail.com>
2026-03-07 14:48:54 -0800

    Added zig config

--- config.c
+++ config.c
@@ -3,15 +3,25 @@
 
 #define NUM_LANGUAGES (sizeof(languages) / sizeof(languages[0]))
 
+// C
 const char *c_types[] = { "int", "char", "float", "double", "bool", "long", "static", "const" };
 const char *c_keywords[] = { "void", "return", "typedef" };
 const char *c_statements[] = { "for", "while", "do", "switch", "case", "if", "else", "break", "continue" };
 const char *c_headers[] = { "include", "define", "ifndef", "endif" };
 
+// Python
 const char *py_types[] = { "True", "False", "None", "int", "str", "float", "list", "dict" };
 const char *py_keywords[] = { "def", "return", "lambda", "yield", "class", "global", "nonlocal", "and", "or", "not", "is", "in" };
 const char *py_statements[] = { "if", "else", "elif", "for", "while", "break", "continue", "pass", "try", "except", "finally", "raise", "assert", "with", "async", "await" };
 const char *py_headers[] = { "import", "from", "as" };
+
+// Zig
+const char *zig_types[] = { "bool", "void", "noreturn", "type", "anyopaque", "anyerror", "usize", "isize", "u8", "u16", "u32", "u64", "u128", "i8", "i16", "i32", "i64", "i128" };
+const char *zig_keywords[] = { "fn", "pub", "const", "var", "struct", "enum", "union", "error", "test", "comptime", "inline", "extern", "export", "packed" };
+const char *zig_statements[] = { "if", "else", "switch", "while", "for", "break", "continue", "return", "try", "catch", "resume", "suspend", "await", "defer", "errdefer", "orelse" 
+};
+const char *zig_headers[] = { "import", "usingnamespace" };
+
 
 
 LanguageConfig c_lang = {
@@ -53,10 +63,24 @@ LanguageConfig py_lang = {
     .single_line_comment = "#"
 };
 
+LanguageConfig zig_lang = {
+    .extention = ".zig",
+    .types = zig_types,
+    .type_count = 18,
+    .keywords = zig_keywords,
+    .keyword_count = 14,
+    .statements = zig_statements,
+    .statement_count = 16,
+    .headers = zig_headers,
+    .header_count = 2,
+    .single_line_comment = "//"
+}; 
+
 LanguageConfig* languages[] = { 
     &c_lang,
     &h_lang,
-    &py_lang
+    &py_lang,
+    &zig_lang
 };
 
 LanguageConfig* get_config(const char *filename) {
